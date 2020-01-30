caa700f56b5f30207f522defdd8f1537a5019b4c
Wendy Harris <wendyv510@hotmail.com>
2020-01-30 00:37:48 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,23 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
-
+  get '/' do 
+    erb :super_hero
+  end 
+  
+  post '/teams' do 
+    @team = Team.new(params[:team])
+    
+    @heroes = Hero.all
+    
+   # @heroes.each do |hero|
+    #   Hero.name 
+     #  Hero.power
+      # Hero.bio
+    #end 
+    
+     
+    
+    erb :team
+  end
 end
